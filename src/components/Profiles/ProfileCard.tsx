7fb5f37307edcc4938036e15cea48d4ddedff01d
Annson <annson@me.com>
2023-04-04 17:57:50 -0500

    add card creation and profiles filter

--- src/components/Profiles/ProfileCard.tsx
+++ src/components/Profiles/ProfileCard.tsx
@@ -1,6 +1,6 @@
 import { MouseEvent } from "react";
 
-export default function ProfileCard() {
+export default function ProfileCard({ profile }) {
   return (
     <div
       className="m-3 h-28 w-96 flex-grow border-spacing-1 overflow-hidden rounded-lg border border-gray-300 p-0 shadow-md shadow-neutral-100 transition-all delay-100 ease-in-out"
@@ -18,15 +18,11 @@ export default function ProfileCard() {
     >
       <div className="border-b-2 bg-gray-50 p-3">
         <p className="m-1 font-medium tracking-wider text-sky-500">
-          Anthony Cheung
+          {profile.name}
         </p>
-        <p className="m-1 text-gray-500">New York</p>
-        <a
-          className="m-1 text-gray-500"
-          target="_blank"
-          href="https://github.com/ranylm"
-        >
-          https://github.com/ranylm
+        <p className="m-1 text-gray-500">{profile.location}</p>
+        <a className="m-1 text-gray-500" target="_blank" href={profile.link}>
+          {profile.link}
         </a>
       </div>
       <div className="LowerProfileDiv m-1 text-left">
--- src/components/Profiles/Profiles.tsx
+++ src/components/Profiles/Profiles.tsx
@@ -1,17 +1,27 @@
 import ProfileCardList from "./ProfileCardList";
+import { useState } from "react";
 
 export default function Profiles() {
+  const [term, setTerm] = useState("");
+
   return (
     <div className="h-full w-full p-4 font-sans">
       <header className="mx-auto text-6xl font-bold">Placeholder Header</header>
       <div className="m-4 min-h-max">
         <div className="m-8 flex flex-col items-center">
-          <span className="-mb-14 text-neutral-400">
+          {/* <span className="-mb-14 text-neutral-400">
             Who are you looking for?
-          </span>
-          <input className="z-10 m-8 w-4/6 rounded-md border border-gray-400 bg-transparent"></input>
+          </span> */}
+          <input
+            type="text"
+            onKeyUp={(e) => {
+              setTerm(e.target.value);
+            }}
+            placeholder="Who are you looking for?"
+            className="z-10 m-8 w-4/6 rounded-md border border-gray-400 bg-transparent placeholder:-mb-14 placeholder:text-center placeholder:text-neutral-400"
+          />
         </div>
-        <ProfileCardList></ProfileCardList>
+        <ProfileCardList term={term} />
       </div>
     </div>
   );
